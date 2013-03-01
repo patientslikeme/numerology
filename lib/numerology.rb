357c67181358da445625ce3aa24aa8f20e000877
Steven Hammond <shammond@northpub.com>
2013-03-01 13:33:09 -0500

    Added significant figures.

--- lib/numerology.rb
+++ lib/numerology.rb
@@ -2,10 +2,17 @@ require "numerology/version"
 
 module Numerology
   def format_like(example)
-    if example =~ /^[0\s]/
-      formatted_string = "%#{example[0]}#{example.gsub(/\D/,'').length}d" % self 
+    if example.gsub(/\D/,'') =~ /(0+$)/
+      sig_fig = $1.length
+      temp = self.round(sig_fig*-1)
     else
-      formatted_string = self.to_s
+      temp = self
+    end
+
+    if example =~ /^[0\s]/
+      formatted_string = "%#{example[0]}#{example.gsub(/\D/,'').length}d" % temp 
+    else
+      formatted_string = temp.to_s
     end
 
     formatted_string = separate_thousands(formatted_string, example[-4]) if example =~ /(\D)\d{3}$/
